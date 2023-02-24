21bc60becf4d117d8f0621962df5ea6141a6fdb7
Kazzzemir <s1ckwell@mail.ru>
2023-02-24 13:48:28 +0300

    final desktop only

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove ('active');
 });
+
+const counters = document.querySelectorAll('.skills__percentage-value');
+const lines = document.querySelectorAll('.skills__percentage-filledBar');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML
+});
