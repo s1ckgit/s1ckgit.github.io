6a8c6da4d321ae991cfea2e11739292d6c229692
Kazzzemir <s1ckwell@mail.ru>
2023-03-01 21:10:38 +0300

    adaptive version_1.0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.menu__close');
+const menu_link = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add ('active');
@@ -9,6 +10,12 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove ('active');
 });
+
+menu_link.forEach(menu_link => menu_link.addEventListener('click', () => {
+    menu.classList.remove ('active');
+}));
+
+
 
 const counters = document.querySelectorAll('.skills__percentage-value');
 const lines = document.querySelectorAll('.skills__percentage-filledBar');
